b3ce6054708d1f8ebb773ac1c3ccd87dc3c19943
yunojang <54504352+yunojang@users.noreply.github.com>
2025-07-15 00:55:25 +0900

    [Gold IV] Title: N-Queen, Time: 11248 ms, Memory: 120156 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -3,33 +3,25 @@ n = int(input())
 c = [0]
 
 
-def search(path, used_cols, used_diag1, used_diag2):
-    y = len(path)  # 현재 행(row) 번호
-    # base case
-    if y == n:
-        # print(path)
+def queen(used_cols, used_dialog1, used_dialog2):
+    row = len(used_cols)
+    if row == n:
         c[0] += 1
         return
 
-    # 현재 row에서 올바른 열번호 찾기
-    for x in range(n):
-        if x in used_cols or (y - x) in used_diag1 or (y + x) in used_diag2:
+    for col in range(n):
+        if col in used_cols or row + col in used_dialog1 or row - col in used_dialog2:
             continue
-        # if any(x == px or abs(py - y) == abs(px - x) for py, px in path):
-        #     continue
 
-        used_cols.add(x)
-        used_diag1.add((y - x))
-        used_diag2.add((y + x))
-        path.append((y, x))
-
-        search(path, used_cols, used_diag1, used_diag2)
-
-        used_cols.remove(x)
-        used_diag1.remove((y - x))
-        used_diag2.remove((y + x))
-        path.pop()
+        used_cols.add(col)
+        used_dialog1.add(row + col)
+        used_dialog2.add(row - col)
+        queen(used_cols, used_dialog1, used_dialog2)
+        used_cols.remove(col)
+        used_dialog1.remove(row + col)
+        used_dialog2.remove(row - col)
 
 
-search([], set(), set(), set())
+queen(set(), set(), set())
+
 print(c[0])
