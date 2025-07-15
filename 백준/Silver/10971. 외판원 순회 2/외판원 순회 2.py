e650d8ed1ebc14b0ffa54026a3b1162cb286f3f1
yunojang <54504352+yunojang@users.noreply.github.com>
2025-07-15 20:06:02 +0900

    [Silver II] Title: 외판원 순회 2, Time: 2376 ms, Memory: 115452 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -11,20 +11,18 @@ min_cost = [float("inf")]
 
 
 def search(path, cur=None, t_cost=0):
-    if len(path) == n + 1:
-        min_cost[0] = min(t_cost, min_cost[0])
+    if len(path) == n:
+        back_cost = costs[cur][path[0]]
 
-    is_last = len(path) == n
+        if back_cost == 0:
+            return
+
+        min_cost[0] = min(t_cost + back_cost, min_cost[0])
+
     for i in range(n):
         cost = costs[cur][i] if cur is not None else -1
 
-        if cost == 0:
-            continue
-
-        if is_last and i != path[0]:
-            continue
-
-        if not is_last and i in path:
+        if i in path or cost == 0:
             continue
 
         search(path + [i], i, t_cost + max(cost, 0))
