5f05aaf4ffcd4a9f08424e872fa94f83614e04c7
yunojang <54504352+yunojang@users.noreply.github.com>
2025-07-17 02:42:33 +0900

    [Silver III] Title: 소수 구하기, Time: 180 ms, Memory: 42624 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,15 +1,31 @@
 s, e = list(map(int, input().split(" ")))
 
 
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+# def is_prime(n):
+#     if n <= 1:
+#         return False
+#     for i in range(2, int(n**0.5) + 1):
+#         if n % i == 0:
+#             return False
+#     return True
 
 
-for i in range(s, e + 1):
-    if is_prime(i):
-        print(i)
+# for i in range(s, e + 1):
+#     if is_prime(i):
+#         print(i)
+
+
+def seiev(s, e):
+    prime = [True] * (e + 1)
+    prime[0] = prime[1] = False
+
+    for i in range(2, int(e**0.5) + 1):
+        if prime[i]:
+            for j in range(i * i, e + 1, i):
+                prime[j] = False
+
+    return [i for i, is_p in enumerate(prime) if is_p and i >= s]
+
+
+for p in seiev(s, e):
+    print(p)
