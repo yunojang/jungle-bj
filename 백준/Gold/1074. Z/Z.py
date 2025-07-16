d6b0a0c32b5b8c62265abc553ffcf8c3bb953dd5
yunojang <54504352+yunojang@users.noreply.github.com>
2025-07-17 00:15:30 +0900

    [Gold V] Title: Z, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -1,32 +1,30 @@
-from time import time
+n, r, c = list(map(int, input().split(" ")))
 
-n, r, c = tuple(map(int, input().split(" ")))
+orders = [(0, 0), (0, 1), (1, 0), (1, 1)]
 
-visited = []
-# cnt = [0]
-ords = [(0, 0), (0, 1), (1, 0), (1, 1)]
+count = 0
 
 
-def search(n, sy=0, sx=0, cnt=0):
+def search(n, y, x):
+    global count
+
+    if y == r and x == c:
+        print(count)
+        return True
     if n == 0:
-        # visited.append((sy, sx))
-        if sy == r and sx == c:
-            print(cnt)
-            return
-            # print(cnt[0])
-        # cnt[0] += 1
-        return
+        count += 1  # n이 0이 되어, 1개의 값을 검색하는 시점에서
+        return False
 
-    # z 순서로 재귀
-    for i, (dy, dx) in enumerate(ords):
+    for dy, dx in orders:
         unit = 2 ** (n - 1)
-        ny, nx = (sy + dy * unit, sx + dx * unit)
-        # ny,nx로 시작하는 공간 범위가 r,c를 포함하지 않으면 푸루닝
-        if ny <= r and nx <= c and ny + unit > r and nx + unit > c:
-            search(n - 1, ny, nx, cnt + (unit**2 * i))
+        ny, nx = y + dy * unit, x + dx * unit
+
+        if ny > r or nx > c or ny + unit <= r or nx + unit <= c:
+            count += unit * unit
+            continue
+
+        if search(n - 1, ny, nx):
+            return True
 
 
-# prev = time()
-search(n)
-# print(time() - prev)
-# print(visited)
+search(n, 0, 0)
