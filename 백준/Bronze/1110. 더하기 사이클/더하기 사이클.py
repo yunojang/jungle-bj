ae6d4ce7f59add8768606c13995e5b2e0eefa496
yunojang <54504352+yunojang@users.noreply.github.com>
2025-07-17 11:11:35 +0900

    [Bronze I] Title: 더하기 사이클, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -2,23 +2,21 @@ import sys
 
 n = int(sys.stdin.readline())
 
-cnt = 0
 
-
-def new_num(num, start=False):
-    global cnt
-    cnt += 1
-
-    if num == n and not start:
-        return
-
+def new_num(num):
     left = num // 10
     right = num % 10
     sum = left + right
     sum_right = sum % 10
 
-    return new_num(int(str(right) + str(sum_right)))
+    return int(str(right) + str(sum_right))
 
 
-new_num(n, True)
-print(cnt - 1)
+cnt = 1
+num = new_num(n)
+
+while num != n:
+    num = new_num(num)
+    cnt += 1
+
+print(cnt)
