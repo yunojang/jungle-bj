e67b8c6780e2061fdb967d0b6e1ce610b35cd153
yunojang <54504352+yunojang@users.noreply.github.com>
2025-07-22 17:59:22 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 188 ms, Memory: 48532 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
@@ -0,0 +1,26 @@
+import sys
+
+inputs = sys.stdin.readlines()
+
+def get_max_rect(hs):
+    max_h = 0
+    mono_inc= []
+    
+    for i in range(len(hs)):
+        while mono_inc and hs[mono_inc[-1]] > hs[i]:
+            poped_idx = mono_inc.pop()
+            width = i if not mono_inc else i - mono_inc[-1] - 1
+            max_h = max(max_h, width * hs[poped_idx])
+        mono_inc.append(i)
+    for idx in mono_inc:
+        l = len(hs) - idx
+        max_h = max(max_h, l * hs[idx])
+    return max_h
+
+
+for input in inputs:
+    if input == "0\n":
+        break
+
+    n, *nums = list(map(int, input.split()))
+    print(get_max_rect(nums + [0]))
