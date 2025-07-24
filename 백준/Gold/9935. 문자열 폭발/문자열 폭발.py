e9ce85c997e3fb4dfd0f340d91081d71663151c5
yunojang <54504352+yunojang@users.noreply.github.com>
2025-07-24 10:56:00 +0900

    [Gold IV] Title: 문자열 폭발, Time: 844 ms, Memory: 43596 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+s = str(input())[0:-1]
+explode_s = str(input())[0:-1]
+explode_l = len(explode_s)
+
+stack = []
+
+for c in s:
+    stack.append(c)
+    while len(stack) >= explode_l and ("".join(stack[len(stack)-explode_l:]) == explode_s):
+        for _ in range(explode_l):
+            stack.pop()
+
+print("".join(stack) if stack else "FRULA")
