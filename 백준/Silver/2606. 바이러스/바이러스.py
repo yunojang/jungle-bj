ee85e5e1ebde3f3fab9296e69176422b809106d9
yunojang <54504352+yunojang@users.noreply.github.com>
2025-07-29 01:53:59 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+n = int(input())
+m = int(input())
+
+graph = {i: [] for i in range(1, n+1)}
+for _ in range(m):
+    s,e  = tuple(map(int,input().split()))
+    graph[s].append(e)
+    graph[e].append(s)
+
+visited = [False] * (n+1)
+
+def visite(cur):
+    cnt = 0
+    if visited[cur]:
+        return 0
+    visited[cur] = True
+    cnt += 1
+    for adj in graph[cur]:
+        cnt += visite(adj)
+    return cnt
+
+print(visite(1) -1)
