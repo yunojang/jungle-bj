7c161cdca596f6a749731b35df91ee8ef45797d5
yunojang <54504352+yunojang@users.noreply.github.com>
2025-07-29 23:00:23 +0900

    [Gold III] Title: 아침 산책, Time: 1008 ms, Memory: 102676 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,36 +1,43 @@
 import sys
+from collections import deque
+
+# sys.setrecursionlimit(10**6)
 
 input = sys.stdin.readline
 
 n = int(input())
-a = "0" + input().strip()
+a = [None] + list(map(int, input().strip()))
 graph = {i: [] for i in range(1, n + 1)}
+edges = []
 
 for _ in range(n - 1):
     s, e = tuple(map(int, input().split()))
+    edges.append((s, e))
     graph[s].append(e)
     graph[e].append(s)
 
 total_route = 0
+for u, v in edges:
+    if a[u] == 1 and a[v] == 1:
+        total_route += 2
+
+visited = [False] * (n + 1)
 for i in range(1, n + 1):
-    if a[i] == "0":
-        continue
-    visited = [False] * (n + 1)
+    if a[i] == 0 and not visited[i]:
+        q = deque([i])
+        visited[i] = True
+        indoor_set = set()
 
-    # cur 에서 나올수있는 경로 수 리턴
-    def dfs(cur, first=False):
-        if visited[cur] and a[cur] == "1":
-            return 0
-        visited[cur] = True
+        while q:
+            x = q.popleft()
+            for adj in graph[x]:
+                if a[adj] == 0 and not visited[adj]:
+                    visited[adj] = True
+                    q.append(adj)
+                elif a[adj] == 1:
+                    indoor_set.add(adj)
 
-        if not first and a[cur] == "1":
-            return 1
-        else:
-            cnt = 0
-            for adj in graph[cur]:
-                cnt += dfs(adj)
-            return cnt
-
-    total_route += dfs(i, True)
+        k = len(indoor_set)
+        total_route += k * (k - 1)
 
 print(total_route)
