dbc44432b3d99da1ae4b758d47ec33e8e6dc7788
yunojang <54504352+yunojang@users.noreply.github.com>
2025-07-30 23:12:16 +0900

    [Gold II] Title: 장난감 조립, Time: 56 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Gold/2637. 장난감 조립/장난감 조립.py
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.py
@@ -1,35 +1,33 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
 
+input = sys.stdin.readline
 n = int(input())
 m = int(input())
-graph = [[] for _ in range(n+1)]
-indegree = [0 for _ in range(n+1)]
-need = [[0] * (n+1) for _ in range(n+1)]
-
+graph = {i: [] for i in range(1, n + 1)}
+indeg = {i: 0 for i in range(1, n + 1)}
 
 for _ in range(m):
-    p, r, c = tuple(map(int, input().split()))
-    graph[r].append((p, c))
-    # graph[u].append(v)
-    indegree[p] += 1
+    v, u, c = tuple(map(int, input().split()))
+    graph[u].append((v, c))
+    indeg[v] += 1
 
+ms = [[0] * (n + 1) for _ in range(n + 1)]
 q = deque()
-for i in range(1, n+1):
-    if indegree[i] == 0:
+for i in range(1, n + 1):
+    if indeg[i] == 0:
         q.append(i)
-        need[i][i] = 1
+        ms[i][i] = 1
 
 while q:
-    cur = q.popleft() # 차수 0
-    for next, cnt in graph[cur]: # 나를 필요로 하는 부품
-        for i in range(1,n+1):
-            need[next][i] += need[cur][i] * cnt
-        indegree[next] -= 1
-        if indegree[next] == 0:
+    cur = q.popleft()
+    for next, cnt in graph[cur]:
+        for i in range(1, n + 1):
+            ms[next][i] += ms[cur][i] * cnt
+        indeg[next] -= 1
+        if indeg[next] == 0:
             q.append(next)
 
-for i in range(n):
-    if need[n][i] > 0:
-        print(i, need[n][i])
+for i in range(1, n + 1):
+    if ms[n][i]:
+        print(i, ms[n][i])
