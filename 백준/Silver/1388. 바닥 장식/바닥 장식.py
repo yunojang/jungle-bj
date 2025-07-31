8745b5addd8aaa5182b7e4fbb37e7c5d755eefe2
yunojang <54504352+yunojang@users.noreply.github.com>
2025-07-31 10:16:04 +0900

    [Silver IV] Title: 바닥 장식, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -0,0 +1,42 @@
+import sys
+
+input = sys.stdin.readline
+n, m = tuple(map(int, input().split()))
+
+maps = []
+for _ in range(n):
+    maps.append(list(input().strip()))
+visited = [[False] * m for _ in range(n)]
+cnt = 0
+
+vd = [(1, 0), (-1, 0)]
+hd = [(0, 1), (0, -1)]
+
+
+def check_same(y, x):
+    if visited[y][x]:
+        return
+    visited[y][x] = True
+
+    if maps[y][x] == "|":
+        for dy, dx in vd:
+            ny, nx = y + dy, x + dx
+            if ny < 0 or ny >= n or maps[ny][nx] == "-":
+                continue
+            check_same(ny, nx)
+    else:
+        for dy, dx in hd:
+            ny, nx = y + dy, x + dx
+            if nx < 0 or nx >= m or maps[ny][nx] == "|":
+                continue
+            check_same(ny, nx)
+
+
+for i in range(n):
+    for j in range(m):
+        if not visited[i][j]:
+            cnt += 1
+            check_same(i, j)
+            visited[i][j] = True
+
+print(cnt)
