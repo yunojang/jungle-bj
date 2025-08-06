15ff25a252056df0f80e671ee468f4ea307e53dd
yunojang <54504352+yunojang@users.noreply.github.com>
2025-08-07 04:09:58 +0900

    [Gold I] Title: 외판원 순회, Time: 320 ms, Memory: 130292 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.py
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -4,28 +4,27 @@ input = sys.stdin.readline
 
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
-
 INF = float("inf")
-cost = [[INF] * n for _ in range(1 << n)]
-cost[1][0] = 0
+dp = [[INF] * n for _ in range(1 << n)]
+dp[1][0] = 0
 
 for visited in range(1, 1 << n):
     for cur in range(n):
-        if not (visited & (1 << cur)):
+        if not visited & (1 << cur):
             continue
         for next in range(n):
-            if visited & (1 << next) or graph[cur][next] == 0:
+            if cur == next or visited & (1 << next) or graph[cur][next] == 0:
                 continue
             next_visited = visited | (1 << next)
-            cost[next_visited][next] = min(
-                cost[next_visited][next], cost[visited][cur] + graph[cur][next]
+            dp[next_visited][next] = min(
+                dp[next_visited][next], dp[visited][cur] + graph[cur][next]
             )
 
-ans = INF
-for i in range(n):
-    if graph[i][0] == 0:
+res = INF
+for end_cur in range(n):
+    if graph[end_cur][0] == 0:
         continue
-    all_visited = (1 << n) - 1
-    ans = min(ans, cost[all_visited][i] + graph[i][0])
+    cost = dp[(1 << n) - 1][end_cur] + graph[end_cur][0]
+    res = min(res, cost)
 
-print(ans)
+print(res)
