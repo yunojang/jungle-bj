9cec49e57c58864b33ec7e5169391c678728ff2a
yunojang <54504352+yunojang@users.noreply.github.com>
2025-08-07 02:56:53 +0900

    [Gold V] Title: 동전, Time: 1260 ms, Memory: 36420 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -1,26 +1,23 @@
 import sys
 
 input = sys.stdin.readline
-
 t = int(input())
 
 for _ in range(t):
     n = int(input())
     coins = list(map(int, input().split()))
-    k = int(input())
+    m = int(input())
+    t = [[0] * (m + 1) for _ in range(n + 1)]
 
-    table = [[0] * (k + 1) for _ in range(n + 1)]
     for i in range(1, n + 1):
         coin = coins[i - 1]
-        for j in range(1, k + 1):
-            if coin > k:
-                table[i][j] = table[i - 1][j]
+        for j in range(1, m + 1):
+            if coin > j:
+                t[i][j] = t[i - 1][j]
             else:
-                table[i][j] += table[i - 1][j]
-                for x in range(1, (j // coin) + 1):
-                    table[i][j] += table[i - 1][j - coin * x]
+                for k in range(j // coin + 1):
+                    t[i][j] += t[i - 1][j - (k * coin)]
                 if j % coin == 0:
-                    table[i][j] += 1
+                    t[i][j] += 1
 
-    cnt = table[n][k]
-    print(cnt)
+    print(t[n][m])
