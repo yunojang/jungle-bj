38c4fa3c86176bfedce580bb3535dedf221f10d4
yunojang <54504352+yunojang@users.noreply.github.com>
2025-08-06 15:17:33 +0900

    [Gold IV] Title: 점프, Time: 308 ms, Memory: 137724 KB -BaekjoonHub

--- 백준/Gold/2253. 점프/점프.py
+++ 백준/Gold/2253. 점프/점프.py
@@ -1,29 +1,25 @@
+# dp 풀이
+# dp[pos][jump] - pos에 jump 거리로 도착 했을 때 최소 이동
 import sys
 from collections import deque
 
 input = sys.stdin.readline
-n, m = tuple(map(int, input().split()))
 
-st = []
+n, m = map(int, input().split())
+stone = set(int(input()) for _ in range(m))
 
-for _ in range(m):
-    st.append(int(input()))
+max_jump = int((2 * n) ** 0.5) + 2
+dp = [[float("inf")] * (max_jump + 1) for _ in range(n + 1)]
+dp[1][0] = 0
 
-visited = set([(0, 0)])
-q = deque([(1, 0, 0)])
-
-res = -1
-while q:
-    l, p, cnt = q.popleft()
-    if l == n:
-        res = cnt
-        break
-    for i in range(3):
-        d = p + (i - 1)
-        next = l + d
-        if d < 1 or (next in st) or ((next, d) in visited):
+for pos in range(1, n + 1):
+    for x in range(max_jump + 1):
+        if dp[pos][x] == float("inf"):  # pos에 x점프로 도달한적 없음
             continue
-        visited.add((next, d))
-        q.append((next, d, cnt + 1))
+        for nj in (x - 1, x, x + 1):
+            np = pos + nj
+            if 1 <= np and np <= n and np not in stone:
+                dp[np][nj] = min(dp[np][nj], dp[pos][x] + 1)
 
-print(res)
+result = min(dp[n])
+print(result if result != float("inf") else -1)
