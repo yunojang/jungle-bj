a91f9b65694dda9b2e97847dd01b2d3aa2e00e72
yunojang <54504352+yunojang@users.noreply.github.com>
2025-12-03 13:32:47 +0900

    [Silver V] Title: 종이자르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.py
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -1,31 +1,27 @@
 import sys
-inputs = sys.stdin.readlines() 
 
-w, h = list(map(int, inputs[0].split(" ")))
-n = int(inputs[1])
-cuts = inputs[2:]
+input = sys.stdin.readline
 
-xs = []
-ys = []
-for cut in cuts:
-    d, v = tuple(map(int, cut.split(" ")))
-    if d == 0:
-        xs.append(v)
+w, h = list(map(int, input().split()))
+n = int(input())
+cuts = [list(map(int, input().split())) for _ in range(n)]
+x_cuts = [0, h]
+y_cuts = [0, w]
+
+for is_y, pos in cuts:
+    if is_y:
+        y_cuts.append(pos)
     else:
-        ys.append(v)
-    
-xs.sort()
-ys.sort()
+        x_cuts.append(pos)
+x_cuts.sort()
+y_cuts.sort()
 
-def get_interval(sorted, end):
-    prev = 0
-    intervals = []
-    for v in sorted + [end]:
-        intervals.append(v - prev)
-        prev = v
-    return intervals
+xmax = 0
+ymax = 0
+for x1, x2 in zip(x_cuts, x_cuts[1:]):
+    xmax = max(xmax, x2 - x1)
+for y1, y2 in zip(y_cuts, y_cuts[1:]):
+    ymax = max(ymax, y2 - y1)
 
-mx = max(get_interval(xs, h))
-my = max(get_interval(ys, w))
 
-print(mx * my)
+print(xmax * ymax)
